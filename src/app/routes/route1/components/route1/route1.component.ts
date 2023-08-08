31558b0dad19fafb6500865dc3bf1808aa443dc0
Daniel Lewis <daniellewis777@gmail.com>
2023-08-08 16:58:42 +0100

    ts

--- src/app/routes/route1/components/route1/route1.component.ts
+++ src/app/routes/route1/components/route1/route1.component.ts
@@ -1,8 +1,16 @@
 import { Component } from '@angular/core'
+import { keys } from 'lodash-es'
 
 @Component({
   selector: 'app-route1',
   templateUrl: './route1.component.html',
   styleUrls: ['./route1.component.scss']
 })
-export class Route1Component {}
+export class Route1Component {
+  keys = keys
+  bsSurname: string
+
+  bsSubmit() {
+    console.log('bsSubmit', this.bsSurname)
+  }
+}
