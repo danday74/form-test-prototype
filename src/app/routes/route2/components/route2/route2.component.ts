5ca3f60598e109c7b43fbbd67e3473016d9c275d
Daniel Lewis <daniellewis777@gmail.com>
2023-08-08 23:22:13 +0100

    route2

--- src/app/routes/route2/components/route2/route2.component.ts
+++ src/app/routes/route2/components/route2/route2.component.ts
@@ -1,8 +1,17 @@
 import { Component } from '@angular/core'
+import { keys } from 'lodash-es'
 
 @Component({
   selector: 'app-route2',
   templateUrl: './route2.component.html',
   styleUrls: ['./route2.component.scss']
 })
-export class Route2Component {}
+export class Route2Component {
+  keys = keys
+
+  model: { surname: string } = { surname: null }
+
+  submit() {
+    console.log('submit', this.model.surname)
+  }
+}
--- src/app/routes/route2/route2.module.ts
+++ src/app/routes/route2/route2.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core'
 import { CommonModule } from '@angular/common'
 import { Route2RoutingModule } from './route2-routing.module'
 import { Route2Component } from './components/route2/route2.component'
+import { FormsModule } from '@angular/forms'
 
 @NgModule({
   declarations: [
@@ -9,7 +10,8 @@ import { Route2Component } from './components/route2/route2.component'
   ],
   imports: [
     CommonModule,
-    Route2RoutingModule
+    Route2RoutingModule,
+    FormsModule
   ]
 })
 export class Route2Module {}
