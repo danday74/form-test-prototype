b99a26a94cb77daaf0622ba4d6523e2bab259ba6
Daniel Lewis <daniellewis777@gmail.com>
2023-08-24 09:58:03 +0100

    app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,13 @@
-import { NgModule } from '@angular/core'
+import { APP_INITIALIZER, NgModule } from '@angular/core'
 import { BrowserModule } from '@angular/platform-browser'
 import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component'
 import { PipesModule } from './pipes/pipes.module'
+import { AppInitService } from './services/app-init.service'
+import { HttpClientModule } from '@angular/common/http'
+import { CssFiltersModule } from './modules/css-filters/css-filters.module'
+
+const appInit = (appInitService: AppInitService) => (): Promise<any> => appInitService.init()
 
 @NgModule({
   declarations: [
@@ -11,9 +16,18 @@ import { PipesModule } from './pipes/pipes.module'
   imports: [
     AppRoutingModule,
     BrowserModule,
-    PipesModule
+    HttpClientModule,
+    PipesModule,
+    CssFiltersModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: APP_INITIALIZER,
+      useFactory: appInit,
+      deps: [AppInitService],
+      multi: true
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
