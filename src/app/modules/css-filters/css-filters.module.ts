4c80629a614a5b78270b0e2382e08d9b9aaf0bcf
Daniel Lewis <daniellewis777@gmail.com>
2023-08-25 01:34:57 +0100

    csf module

--- src/app/modules/css-filters/css-filters.module.ts
+++ src/app/modules/css-filters/css-filters.module.ts
@@ -3,10 +3,12 @@ import { CommonModule } from '@angular/common'
 import { CssFiltersComponent } from './components/css-filters/css-filters.component'
 import { FormsModule } from '@angular/forms'
 import { DirectivesModule } from '../../directives/directives.module'
+import { CssFiltersDialogComponent } from './components/css-filters-dialog/css-filters-dialog.component'
 
 @NgModule({
   declarations: [
-    CssFiltersComponent
+    CssFiltersComponent,
+    CssFiltersDialogComponent
   ],
   exports: [
     CssFiltersComponent
