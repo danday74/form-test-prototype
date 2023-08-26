b1ded485b0536d8cb9116e8c3bd9667049528944
Daniel Lewis <daniellewis777@gmail.com>
2023-08-26 05:15:16 +0100

    default css filters

--- src/app/modules/css-filters/data/default-css-filters.ts
+++ src/app/modules/css-filters/data/default-css-filters.ts
@@ -1,13 +1,58 @@
 import { ICssFilters } from '../../../interfaces/i-css-filters'
+import { keys } from 'lodash-es'
+import { TCssFilter } from '../../../types/t-css-filter'
 
-export const defaultCssFilters: ICssFilters = {
-  brightness: 100,
-  contrast: 100,
-  grayscale: 0,
-  saturate: 100,
-  sepia: 0,
-  'hue-rotate': 0,
-  invert: 0,
-  blur: 0,
-  opacity: 100
+const cssFilters: ICssFilters = {
+  brightness: {
+    name: 'brightness',
+    value: 100,
+    order: null
+  },
+  contrast: {
+    name: 'contrast',
+    value: 100,
+    order: null
+  },
+  grayscale: {
+    name: 'grayscale',
+    value: 0,
+    order: null
+  },
+  saturate: {
+    name: 'saturate',
+    value: 100,
+    order: null
+  },
+  sepia: {
+    name: 'sepia',
+    value: 0,
+    order: null
+  },
+  'hue-rotate': {
+    name: 'hue-rotate',
+    value: 0,
+    order: null
+  },
+  invert: {
+    name: 'invert',
+    value: 0,
+    order: null
+  },
+  blur: {
+    name: 'blur',
+    value: 0,
+    order: null
+  },
+  opacity: {
+    name: 'opacity',
+    value: 100,
+    order: null
+  }
 }
+
+for (const [idx, filter] of keys(cssFilters).entries()) {
+  const fltr: TCssFilter = filter as TCssFilter
+  cssFilters[fltr].order = idx
+}
+
+export const defaultCssFilters: ICssFilters = cssFilters
