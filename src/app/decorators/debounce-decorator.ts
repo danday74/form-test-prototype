bb62c318b04b277e707d231667be4327c1655798
Daniel Lewis <daniellewis777@gmail.com>
2023-08-26 11:11:18 +0100

    decorators

---
+++ src/app/decorators/debounce-decorator.ts
@@ -0,0 +1,21 @@
+import { debounce, DebounceSettings } from 'lodash-es'
+
+// https://engineering.datorama.com/be-aware-of-the-debounce-decorator-6fb24a6d8d5
+
+// example: decorate method with ... @debounceDecorator(250, { leading: true, trailing: false })
+// noinspection JSUnusedGlobalSymbols
+export const debounceDecorator = (waitMs = 0, options: DebounceSettings = { leading: false, trailing: true }) => {
+  return (target: any, propertyKey: string, descriptor: PropertyDescriptor) => {
+    const map = new WeakMap()
+    const originalMethod = descriptor.value
+    descriptor.value = function (...args: any[]) {
+      let debounced = map.get(this)
+      if (!debounced) {
+        debounced = debounce(originalMethod, waitMs, options).bind(this)
+        map.set(this, debounced)
+      }
+      debounced(...args)
+    }
+    return descriptor
+  }
+}
---
+++ src/app/decorators/throttle-decorator.ts
@@ -0,0 +1,21 @@
+import { throttle, ThrottleSettings } from 'lodash-es'
+
+// https://engineering.datorama.com/be-aware-of-the-debounce-decorator-6fb24a6d8d5
+
+// example: decorate method with ... @throttleDecorator(50, { leading: true, trailing: true })
+// noinspection JSUnusedGlobalSymbols
+export const throttleDecorator = (waitMs = 0, options: ThrottleSettings = { leading: true, trailing: true }) => {
+  return (target: any, propertyKey: string, descriptor: PropertyDescriptor) => {
+    const map = new WeakMap()
+    const originalMethod = descriptor.value
+    descriptor.value = function (...args: any[]) {
+      let throttled = map.get(this)
+      if (!throttled) {
+        throttled = throttle(originalMethod, waitMs, options).bind(this)
+        map.set(this, throttled)
+      }
+      throttled(...args)
+    }
+    return descriptor
+  }
+}
