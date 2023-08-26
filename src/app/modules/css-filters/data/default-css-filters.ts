96ad85c95be09193a18d0f8e94514a9af0e7a73e
Daniel Lewis <daniellewis777@gmail.com>
2023-08-26 15:06:43 +0100

    uoms done

--- src/app/modules/css-filters/data/default-css-filters.ts
+++ src/app/modules/css-filters/data/default-css-filters.ts
@@ -1,52 +1,62 @@
 import { ICssFilters } from '../interfaces/i-css-filters'
 import { keys } from 'lodash-es'
 import { TCssFilter } from '../types/t-css-filter'
+import { csfUoms } from './csf-uoms'
 
 const cssFilters: ICssFilters = {
   brightness: {
     name: 'brightness',
     value: 100,
-    order: null
+    order: null,
+    uom: csfUoms.percent
   },
   contrast: {
     name: 'contrast',
     value: 100,
-    order: null
+    order: null,
+    uom: csfUoms.percent
   },
   grayscale: {
     name: 'grayscale',
     value: 0,
-    order: null
+    order: null,
+    uom: csfUoms.percent
   },
   saturate: {
     name: 'saturate',
     value: 100,
-    order: null
+    order: null,
+    uom: csfUoms.percent
   },
   sepia: {
     name: 'sepia',
     value: 0,
-    order: null
+    order: null,
+    uom: csfUoms.percent
   },
   'hue-rotate': {
     name: 'hue-rotate',
     value: 0,
-    order: null
+    order: null,
+    uom: csfUoms.degrees
   },
   invert: {
     name: 'invert',
     value: 0,
-    order: null
+    order: null,
+    uom: csfUoms.percent
   },
   blur: {
     name: 'blur',
     value: 0,
-    order: null
+    order: null,
+    uom: csfUoms.pixels
   },
   opacity: {
     name: 'opacity',
     value: 100,
-    order: null
+    order: null,
+    uom: csfUoms.percent
   }
 }
 
--- src/app/modules/css-filters/interfaces/i-css-filter.ts
+++ src/app/modules/css-filters/interfaces/i-css-filter.ts
@@ -1,7 +1,9 @@
 import { TCssFilter } from '../types/t-css-filter'
+import { ICsfUom } from './i-csf-uom'
 
 export interface ICssFilter {
   name: TCssFilter
   value: number
   order: number
+  uom: ICsfUom
 }
