8055e011c4e7104c90ec018fd3ff5904a86c709d
Daniel Lewis <daniellewis777@gmail.com>
2023-08-27 04:20:05 +0100

    target done

--- src/app/modules/css-filters/components/css-filters/css-filters.component.ts
+++ src/app/modules/css-filters/components/css-filters/css-filters.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, HostListener, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core'
+import { AfterViewInit, Component, ElementRef, HostListener, Input, OnChanges, OnDestroy, OnInit, SimpleChanges } from '@angular/core'
 import { ICssFilters } from '../../interfaces/i-css-filters'
 import { TCssFilter } from '../../types/t-css-filter'
 import { defaultCssFilters } from '../../data/default-css-filters'
@@ -9,14 +9,16 @@ import { csfStorageKeys } from '../../data/csf-storage-keys'
 import { ICssFilter } from '../../interfaces/i-css-filter'
 import { getCsfOrder } from '../../utils/get-csf-order'
 import { throttleDecorator } from '../../../../decorators/throttle-decorator'
+import { differenceInMilliseconds } from 'date-fns'
 
 @Component({
   selector: 'app-css-filters',
   templateUrl: './css-filters.component.html',
   styleUrls: ['./css-filters.component.scss']
 })
-export class CssFiltersComponent implements OnInit, OnChanges {
+export class CssFiltersComponent implements OnInit, AfterViewInit, OnChanges, OnDestroy {
   @Input() width = '100%'
+  @Input() target = false // enable targeting?
 
   min: number
   max: number
@@ -25,8 +27,10 @@ export class CssFiltersComponent implements OnInit, OnChanges {
 
   filters: ICssFilters = this.storageService.getItem(csfStorageKeys.filters, cloneDeep(defaultCssFilters))
   filter: TCssFilter = this.storageService.getItem(csfStorageKeys.filter, findKey(defaultCssFilters, { order: 0 }))
+  targetValue: boolean = this.storageService.getItem(csfStorageKeys.targetValue, false)
 
   private readonly closeOnClick = false
+  private interval: number
 
   constructor(private storageService: StorageService, private ref: ElementRef) {}
 
@@ -41,12 +45,34 @@ export class CssFiltersComponent implements OnInit, OnChanges {
     this.applyFilters()
   }
 
+  ngAfterViewInit() {
+    const targetImagesOnly: boolean = this.targetImagesOnly()
+    if (targetImagesOnly) {
+      this.whenImagesAreReady().then((/* msg: string */) => {
+        this.applyFilters()
+        // console.log(msg)
+      }).catch((/* msg: string */) => {
+        // console.warn(msg)
+      })
+    }
+  }
+
   ngOnChanges(changes: SimpleChanges) {
     if (changes.width && !changes.width.firstChange) this.fixUnits()
+    if (changes.target && !changes.target.firstChange) this.applyFilters()
+  }
+
+  ngOnDestroy() {
+    clearInterval(this.interval)
   }
 
   toggleDialog() {
     this.showDialog = !this.showDialog
+  }
+
+  updateTarget(targetValue: boolean) {
+    this.targetValue = targetValue
+    this.applyFilters()
   }
 
   updateOrder(order: TCssFilter[]) {
@@ -91,10 +117,25 @@ export class CssFiltersComponent implements OnInit, OnChanges {
       return acc
     }, '').trim()
 
-    const html: HTMLHtmlElement = document.querySelector('html')
-    html.style.setProperty('filter', filter)
+    this.applyFiltersToDom(filter)
 
     this.storageService.setItem(csfStorageKeys.filters, this.filters)
+    this.storageService.setItem(csfStorageKeys.targetValue, this.targetValue)
+  }
+
+  private applyFiltersToDom(filter: string) {
+
+    const html: HTMLHtmlElement = document.querySelector('html')
+    const imagez: NodeListOf<HTMLImageElement> = document.querySelectorAll('img')
+    const images: HTMLImageElement[] = Array.from(imagez)
+
+    const targetImagesOnly: boolean = this.targetImagesOnly()
+
+    const htmlFilter: string = targetImagesOnly ? null : filter
+    const imageFilter: string = targetImagesOnly ? filter : null
+
+    html.style.setProperty('filter', htmlFilter)
+    images.forEach((image: HTMLImageElement) => image.style.setProperty('filter', imageFilter))
   }
 
   private doChangeFilter(filter: TCssFilter) {
@@ -105,11 +146,37 @@ export class CssFiltersComponent implements OnInit, OnChanges {
     this.storageService.setItem(csfStorageKeys.filter, filter)
   }
 
+  private targetImagesOnly(): boolean {
+    return this.target && this.targetValue
+  }
+
   private closeDialog(force = false) {
     if (this.closeOnClick || force) this.showDialog = false
   }
 
   private fixUnits() {
     this.width = fixUnits(this.width)
+  }
+
+  private whenImagesAreReady(): Promise<string> {
+    const startTime: Date = new Date()
+    const abortTimeout = 500
+
+    return new Promise((resolve, reject) => {
+      let attempts = 0
+
+      this.interval = setInterval(() => {
+        attempts++
+        const imagez: NodeListOf<HTMLImageElement> = document.querySelectorAll('img')
+        const images: HTMLImageElement[] = Array.from(imagez)
+        if (images.length) {
+          clearInterval(this.interval)
+          resolve(`success - found some images on attempt ${attempts}`)
+        } else if (differenceInMilliseconds(new Date(), startTime) > abortTimeout) {
+          clearInterval(this.interval)
+          reject(`failure - given up looking for images on attempt ${attempts}`)
+        }
+      }, 25)
+    })
   }
 }
