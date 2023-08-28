aaa393d5537c40ded830fd51085a8faceac5014d
Daniel Lewis <daniellewis777@gmail.com>
2023-08-28 22:27:30 +0100

    remove link

--- src/app/services/app-init.service.ts
+++ src/app/services/app-init.service.ts
@@ -18,7 +18,19 @@ export class AppInitService {
 
   private initTheme() {
     const theme: TTheme = this.themeService.getTheme()
-    this.themeService.activateTheme(theme).then()
+    this.themeService.activateTheme(theme).then((theme: TTheme) => {
+      if (theme != null) {
+        setTimeout(() => {
+          const removeLink: HTMLLinkElement = document.querySelector('head > link[data-remove]')
+          if (removeLink) {
+            document.head.removeChild(removeLink)
+          } else {
+            // <link rel="stylesheet" type="text/css" href="/assets/themes/init-theme.css" data-remove>
+            throw Error('head > link[data-remove] avoids page jump on initial page load but it no longer exists')
+          }
+        }, 200)
+      }
+    })
   }
 
   private initLod() {
